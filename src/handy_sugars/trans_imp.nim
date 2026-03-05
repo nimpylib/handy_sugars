a1a86ebae4ada168bcc2093536a3173134d0077d
litlighilit <litlighilit@foxmail.com>
2026-03-05 14:42:00 +0800

    chore(doc)

--- src/handy_sugars/trans_imp.nim
+++ src/handy_sugars/trans_imp.nim
@@ -2,6 +2,18 @@
 import std/macros
 proc dotSlash(id: NimNode): NimNode = prefix(id, "./")
 macro impExp*(pre, names) =
+  ##[
+    ```Nim
+    impExp module, [
+      submod1, submod2,
+    ]
+    ```
+    ->
+    ```Nim
+    import module/[submod1, submod2]
+    export submod1, submod2
+    ```
+  ]##
   result = newStmtList()
   let imp = infix(pre, "/", names)
   result.add nnkImportStmt.newTree(imp)
@@ -9,6 +21,10 @@ macro impExp*(pre, names) =
   for name in names:
     exp.add name
   result.add exp
+
 macro impExpCwd*(pre, names) =
+  ## like `impExp`_ but prefix `pre` with `./`
+  ##
+  ## `impExpCwd module, xxx` -> `import ./module/xxx; export xxx
   getAst impExp(dotSlash(pre), names)
 
