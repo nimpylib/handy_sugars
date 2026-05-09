955e899868486f874a9c458be52ec2b8273277b6
litlighilit <litlighilit@foxmail.com>
2026-05-09 14:56:34 +0800

    fixup(76e1f25): bit_length wrong for negative

--- src/handy_sugars/private/bit_length_util.nim
+++ src/handy_sugars/private/bit_length_util.nim
@@ -3,10 +3,10 @@ import std/bitops
 export popcount
 const BitPerByte = 8
 proc bit_length*(self: SomeInteger): int =
+  template abs_v: untyped =
+    when self is SomeSignedInt: abs(self)
+    else: self
   when defined(noUndefinedBitOpts):
-    1 + fastLog2(
-      when self is SomeSignedInt: abs(self)
-      else: self
-    )
+    1 + fastLog2(abs_v)
   else:
-    sizeof(self) * BitPerByte - bitops.countLeadingZeroBits self
+    sizeof(self) * BitPerByte - bitops.countLeadingZeroBits abs_v
